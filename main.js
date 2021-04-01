e39fbb8390b41c50b014a4fec5e2338d0df1d733
devstackq <loginpsn@mail.ru>
2021-04-01 22:15:47 +0600

    refactor project v2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { mapGame } from "./map.js";
 let p = document.getElementById('pacman')
 let obj = {
-    indexDom: 674,
+    indexDom: 658,
     posDiv: 0,
     rafId: 0,
     keys: {
@@ -17,8 +17,8 @@ let obj = {
     y: "",
     h: 31, // height each block
     size: 28,
-    cool: 0,
     inPlay: false,
+    cool: 0,
 }
 document.addEventListener("DOMContentLoaded", () => {
     obj.pacman = p
@@ -41,79 +41,88 @@ document.addEventListener('keydown', (e) => {
     if (!obj.inPlay) {
         obj.inPlay = true
         obj.pacman.style.display = 'block'
+        p.style.position='absolute'
         obj.rafId = requestAnimationFrame(step)
     }
 })
 
 // define parent,
 // if pacman pos x, y ==
-//     plavno - peremeshat RAF
-correct - > show index - y - , & y
-correct set position in pacaman in Map
 
-todo logic - pacman ConvolverNode, x 420 / 30 + y 660 = 674 index Dom,
-    if obj.grid.children[indexDom].type == 1, no tanfosrTransalte,
-    if type == 0, replace value this dom elems,
-    class freepath, add score, type = 9
+// correct - > show index - y - , & y
+// correct set position in pacaman in Map
+
+// todo logic - pacman ConvolverNode, x 420 / 30 + y 660 = 674 index Dom,
+//     if obj.grid.children[indexDom].type == 1, no tanfosrTransalte,
+//     if type == 0, replace value this dom elems,
+//     class freepath, add score, type = 9
+set bound in Map - for pacman, if type 0, score+=10, etc 
+//computing posit -> inside func, not Dom
+//get posx, posy -> get in mapGame - by Index - type value,  if type ==1, etc
+
+//if currPos == freePath || coin && currPos == pacman width -20px, -> goToDirect()
 
 
+const  step = ()=> {
 
-function step() {
+    let w = 20 / 2
+    let h = 20 / 2
+    let diff = 10
+    // let speed = 2, add 2 px, withou cooldown in RAF
+    //ideas #12 if currPos objX < 15 -> mod = objX % 30, objX-=mod else objX += mod
 
-    let w = 24 / 2
-    let h = 24 / 2
-    let size = 0
+    let speed = 30
 
     //dom index, by X ?
     //obj.grid.children[obj.posX / 30 * obj.posY / 30]
-    if (obj.inPlay) {
 
-        obj.cool--
-            if (obj.cool < 0) {
-                if (obj.posX - w >= 0) {
-                    obj.indexDom = obj.posY + (obj.posX / 30) - 1
-                    console.log(Math.floor(obj.indexDom), 'index dom', obj.posX, obj.posY, 'pos pacman x,y', obj.grid.children[Math.floor(obj.indexDom)], 'dom elem by index')
-                        //if call 6, -1,+1 -28 +28
-                    if (obj.keys.ArrowLeft) { //500 - 50
-                        size = 30
+    console.log(obj.indexDom, obj.posX, obj.posY)
+
+    if (obj.inPlay) {
+                    //-1 ?
+                    obj.cool--
+                    if(obj.cool < 0) {
+                    // console.log(Math.floor(obj.indexDom), 'index dom', obj.posX, obj.posY, 'pos pacman x,y', obj.grid.children[Math.floor(obj.indexDom)], 'dom elem by index')
+                    if (obj.keys.ArrowLeft) {
+                        if (obj.posX >= 38 ) {
+                            //set pacan correct position
+                            obj.indexDom = Math.floor(obj.posY + (obj.posX / 30) )
                             //in Dom elements, 868
                             // if (obj.grid.children[obj.posX] && obj.grid.children[obj.posY]) {
                             // }
-                            // p.style.position = 'absolute'
-                        p.style.transform = `translate(${obj.posX-= size}px, ${obj.posY}px  )`;
-                    }
+                        p.style.transform = `translate(${obj.posX-= speed}px, ${obj.posY}px  )`;
+                       }
                 }
-
-                if (obj.posX + w <= 840) {
-                    obj.indexDom = obj.posY + (obj.posX / 30) + 1
+//840-10 < 840,
                     if (obj.keys.ArrowRight) {
-                        size = 30
-                        p.style.transform = `translate(${obj.posX+=size}px, ${ obj.posY}px )`;
+                        if ( obj.posX  <= 782 ) {
+                    obj.indexDom = Math.floor(obj.posY + (obj.posX / 30))
+                        p.style.transform = `translate(${obj.posX+=speed}px, ${ obj.posY}px )`;
                     }
                 }
                 // console.log(obj.posX, obj.posY, 'bef', obj.pacman)
-                if (obj.posY - h >= 0) {
-                    obj.indexDom = obj.posY + (obj.posX / 30) - 28
                     if (obj.keys.ArrowUp) {
-                        size = 30
-                        p.style.transform = `translate(${obj.posX}px, ${ obj.posY-=size}px )`;
+                        if ( obj.posY   >= 41  ) {
+                    obj.indexDom = Math.floor(obj.posY + (obj.posX / 30) - 28)  
+                        p.style.transform = `translate(${obj.posX}px, ${ obj.posY-=speed}px )`;
                     }
                 }
-
-                if (obj.posY + w <= 930) {
-                    obj.indexDom = obj.posY + (obj.posX / 30) + 28
+                //1 vlock 30px, + height pacman 20px, 930 - 50
                     if (obj.keys.ArrowDown) {
-                        size = 30
-                        p.style.transform = `translate(${obj.posX}px, ${ obj.posY+=size }px )`;
+                        if ( obj.posY  < 870  ) {
+                    obj.indexDom = Math.floor( obj.posY + (obj.posX / 30) + 28)
+                        p.style.transform = `translate(${obj.posX}px, ${ obj.posY+=speed }px )`;
                     }
                 }
-                obj.cool = 60
+            obj.cool = 6
             }
-            // console.log(obj.cool)
 
-        obj.rafId = requestAnimationFrame(step);
+            obj.rafId = requestAnimationFrame(step);
     }
 }
+
+//currPos 870 % 30  != 0, if currPos  % 30 < 15, = currPos -=15 else currPos- currPos% 30 + 30
+
 
 const createBoard = () => {
     //create each block -> get data from mapGame array
