e7054e1f4052a65ecffc2f4622bbb666c53f2cd7
devstackq <loginpsn@mail.ru>
2021-06-27 18:41:17 +0600

    fix scoreboard

--- main.go
+++ main.go
@@ -53,9 +53,22 @@ func CalculateRank(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 
 	case "GET":
+
+		r.Header.Add("Accept-Charset", "UTF-8;q=1, ISO-8859-1;q=0")
 		w.WriteHeader(200)
+		js, err := json.Marshal(byteValue)
+		if err != nil {
+			http.Error(w, string(js), 400)
+			return
+		}
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(byteValue)
+
 	case "POST":
 		last := Score{}
 		b, err := ioutil.ReadAll(r.Body)
